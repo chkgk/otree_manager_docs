63b7bc9bf7299d49fa649303e560e7b9cb99f1e4
Christian König <koenig.kersting@gmail.com>
2018-10-04 13:10:15 +0200

    changed name to ascii

--- conf.py
+++ conf.py
@@ -20,8 +20,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'oTree Manager'
-copyright = '2018, Christian König-Kersting'
-author = 'Christian König-Kersting'
+copyright = '2018, Christian Koenig-Kersting'
+author = 'Christian Koenig-Kersting'
 
 # The short X.Y version
 version = ''
